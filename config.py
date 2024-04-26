1147b3d506a6210b5dbd1c9d08ca665a6d8ae20b
gurpratap1995singh <71231586+gurpratap1995singh@users.noreply.github.com>
2024-04-26 23:57:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-custombucket = "destinationtxt"
+custombucket = "guru-destinationtxt"
 table = "intellipaat"
-databasehost = "database-1.c14bnmt8jlz0.us-east-1.rds.amazonaws.com"
+databasehost = "intellipaat.czww0kw26qcu.us-west-2.rds.amazonaws.com"
 duser = "intel"
 dpass = "intel123"
 s3database = "intellipaat"
